3aa58e5e9a49ac5d3403835b40cf6cb279db9147
KevinRoig <70197762+KevinRoig@users.noreply.github.com>
2020-09-30 20:40:04 +0200

    Create code.php

---
+++ code.php
@@ -0,0 +1,26 @@
+//Link to screenshot:
+//https://zupimages.net/viewer.php?id=20/40/417p.png
+
+//code needed:
+<?php 
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Killer Kevin" => [
+        "city" => "Tours city, baby!",
+        "weapon" => " A fuckin' big knife"
+    ]
+];
+
+
+var_dump($characters);
